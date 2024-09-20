5428c177325d343a8bb7bbb94bbcdcbcaf800e83
Gnlow <gnlow@protonmail.com>
2024-09-20 09:23:52 +0900

    Update Polynomial.ts

--- src/Polynomial.ts
+++ src/Polynomial.ts
@@ -1,3 +1,7 @@
+type Brand<Base, Id> = Base | (Base & { __: Id })
+type X = Brand<number, "X">
+type Y = Brand<number, "Y">
+
 export class Polynomial {
     coeffs
     constructor(coeffs: number[]) {
@@ -10,11 +14,28 @@ export class Polynomial {
                 .slice(1)
         )
     }
-    at(x: number) {
+    at(x: X) {
         return this.coeffs.reduce(
             (sum, c, i) =>
                 sum + c * x**i,
             0,
-        )
+        ) as Y
+    }
+}
+
+export class Quadratic extends Polynomial {
+    declare coeffs: [number, number, number]
+
+    solve(y = 0) {
+        const [c, b, a] = this.coeffs
+        const p = b^2 - 4*a*(c-y)
+        if (p > 0) {
+            return [
+                (-b + p)/(2*a),
+                (-b - p)/(2*a),
+            ] as X[]
+        } else {
+            return [(-b + p)/(2*a)] as X[]
+        }
     }
 }
