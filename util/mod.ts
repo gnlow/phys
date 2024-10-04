cb2e5433b0cec441f582d1fba104e7f96b67f0c5
Gnlow <gnlow@protonmail.com>
2024-10-04 11:10:24 +0900

    Trig fn for deg

--- util/mod.ts
+++ util/mod.ts
@@ -8,6 +8,22 @@ export const radToDeg =
 (rad: number) =>
     rad / Math.PI * 180
 
+export const sin =
+(deg: number) =>
+    Math.sin(degToRad(deg))
+
+export const cos =
+(deg: number) =>
+    Math.cos(degToRad(deg))
+
+export const asin =
+(n: number) =>
+    radToDeg(Math.asin(n))
+
+export const acos =
+(n: number) =>
+    radToDeg(Math.acos(n))
+
 export namespace Deg {
     export const lift =
     (f: (rad: number) => number) =>
