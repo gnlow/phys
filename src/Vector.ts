a34e47fca269573072171b4232b9d0fafb3547e5
Gnlow <gnlow@pm.me>
2025-02-20 08:18:02 +0900

    vector utils

--- src/Vector.ts
+++ src/Vector.ts
@@ -34,6 +34,47 @@ export class Vector {
     }
 }
 
+export class Vec1 extends Vector {
+    get x() { return this.values[0] }
+    set x(x: number) { this.values[0] = x }
+
+    get r() { return this.values[0] }
+    set r(r: number) { this.values[0] = r }
+}
+
+export class Vec2 extends Vec1 {
+    get y() { return this.values[1] }
+    set y(y: number) { this.values[1] = y }
+
+    get g() { return this.values[1] }
+    set g(g: number) { this.values[1] = g }
+}
+
+export class Vec3 extends Vec2 {
+    get z() { return this.values[2] }
+    set z(z: number) { this.values[2] = z }
+
+    get b() { return this.values[2] }
+    set b(b: number) { this.values[2] = b }
+}
+
+export class Vec4 extends Vec3 {
+    get w() { return this.values[3] }
+    set w(w: number) { this.values[3] = w }
+
+    get a() { return this.values[3] }
+    set a(a: number) { this.values[3] = a }
+}
+
+export const vec1 = (x: number) => new Vec1([x])
+export const vec2 = (x: number, y: number) => new Vec2([x, y])
+export const vec3 =
+    (x: number, y: number, z: number) =>
+        new Vec3([x, y, z])
+export const vec4 =
+(x: number, y: number, z: number, w: number) =>
+    new Vec4([x, y, z, w])
+
 export const v =
 (...values: number[]) =>
     new Vector(values)
